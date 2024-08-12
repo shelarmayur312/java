dcdf7c1f3a5f1b9908b3de0f070e374a2fd92b04
shelarmayur312 <156174171+shelarmayur312@users.noreply.github.com>
2024-08-12 02:08:25 -0700

    Add files via upload

---
+++ switch_statement.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+class switch_statement
+{
+public static void main(String args[])
+{
+char color ='g';
+switch (color)
+{
+case 'r':System.out.println("Red");
+case 'g':System.out.println("Green");
+case 'b':System.out.println("Blue");
+
+default : System.out.println("No color found n this color");
+}
+}
+}
