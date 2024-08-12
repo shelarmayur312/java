49660a453f08052d1a9872366f6f8e9c5b32a857
shelarmayur312 <156174171+shelarmayur312@users.noreply.github.com>
2024-08-12 02:32:36 -0700

    Add files via upload

---
+++ One_dimentional_array.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+class One_dimentional_array
+{
+public static void main(String args[])
+{
+int arr[]={05,34,67,78,23};
+for(int i=0;i<5;i++)
+{
+System.out.println(arr[i]);
+}
+}
+}
